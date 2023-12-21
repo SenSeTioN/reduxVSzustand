29b136888734d1abe0a08a8bd223ac3bbe64cef0
SenSeTioN <lankovedik@gmail.com>
2023-12-21 19:11:41 +0300

    add devtools

--- src/stores/zustand.store.ts
+++ src/stores/zustand.store.ts
@@ -1,4 +1,5 @@
 import { create } from 'zustand'
+import { devtools } from 'zustand/middleware'
 
 export interface CounterStateZustand {
   value: number
@@ -9,15 +10,18 @@ export interface CounterStateZustand {
   incrementByAmount: (amount: number) => void
 }
 
-export const useCounterZustand = create<CounterStateZustand>((set) => ({
-  value: 0,
-  title: '',
-  fetchToDo: async () => {
-    const response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
-    const data = await response.json()
-    set({ title: data.title })
-  },
-  increment: () => set((state) => ({ value: state.value + 1 })),
-  decrement: () => set((state) => ({ value: state.value - 1 })),
-  incrementByAmount: (amount: number) => set((state) => ({ value: state.value + amount })),
-}))
+export const useCounterZustand = create<CounterStateZustand>()(
+  devtools((set) => ({
+    value: 0,
+    title: '',
+    fetchToDo: async () => {
+      const response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+      const data = await response.json()
+      set({ title: data.title }, false, 'fetchToDo')
+    },
+    increment: () => set((state) => ({ value: state.value + 1 }), false, 'increment'),
+    decrement: () => set((state) => ({ value: state.value - 1 }), false, 'decrement'),
+    incrementByAmount: (amount: number) =>
+      set((state) => ({ value: state.value + amount }), false, 'incrementByAmount'),
+  }))
+)
